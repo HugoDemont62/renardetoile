9b0397351444492b680d2de4e2e2ab93c0163abc
HugoDemont <hugodemont62@hotmail.fr>
2025-11-14 09:40:49 +0100

    refactor: replace React rendering with custom engine initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Engine } from './lib/Engine.ts'
+import { Scene } from './lib/Scene.ts'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const engine = new Engine(document.body)
+engine.setScene(Scene)
